d1723b8507f186eb9da3f8f77fdaba3518020b71
Oliver Levay <oliver.levay@hotmail.com>
2022-11-21 19:39:46 +0100

    Update payment-callback

--- pages/api/webshop/payment-callback.ts
+++ pages/api/webshop/payment-callback.ts
@@ -4,13 +4,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   if (req.method !== 'POST') {
     res.status(404);
   }
-  console.log('Recieved callback from swish!');
-  console.log('req.body', req.body);
-  console.log('req', req);
-  console.log('req.headers', req.headers);
+  const { status, id } = req.body;
 
-  return res.status(200).json({
-    res: JSON.stringify(res),
-    req: JSON.stringify(req),
-  });
+  console.log('Payment status', status, id);
+
+  return res.status(200).json({ status, id });
 }
